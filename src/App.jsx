b716f69b3b9ed1bac05f5cc7eca41efba2db4d6d
injamamulhak0804 <injamamulhak080404@gmail.com>
2026-02-22 11:02:39 +0530

    feat: add side bar and main content and rightsidebar in main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
-import Sidebar from "./component/ui/Sidebar"
+import { useState } from "react";
+import Sidebar from "./component/ui/Sidebar";
+import CreateCanavaPage from "./pages/createCanava/CreateCanavaPage";
+import RightSideBar from "./component/ui/RightSideBar";
 
 function App() {
+  const [active, setActive] = useState("canava");
+  const [color, setColor] = useState("#FFC0CB");
+  const [rectangles, setRectangles] = useState([]);
+  const [selectedCom, SetSelectedCom] = useState(null);
+
+  // console.log("Rec: ", rectangles);
 
   return (
-    <>   
-      <Sidebar /> 
+    <>
+      <div className="grid grid-cols-12 w-full h-full overflow-y-hidden">
+        <div className="col-span-1">
+          <Sidebar active={active} setActive={setActive} />
+        </div>
+        {active == "canava" && (
+          <CreateCanavaPage
+            color={color}
+            rectangles={rectangles}
+            setRectangles={setRectangles}
+            selectedCom={selectedCom}
+            SetSelectedCom={SetSelectedCom}
+          />
+        )}
+        <RightSideBar
+          color={color}
+          setRectangles={setRectangles}
+          rectangles={rectangles}
+          setColor={setColor}
+          selectedCom={selectedCom}
+        />
+      </div>
     </>
-  )
+  );
 }
-
-export default App
+{
+  /* { id: "canava", icon: "C", label: "Create Canva", top: true },
+  { id: "data", icon: icons.database, label: "Data", top: true },
+  { id: "users", icon: icons.user, label: "Users", top: true },
+  { id: "schedule", icon: icons.calendar, label: "Schedule", top: true },
+  { id: "actions", icon: icons.bolt, label: "Actions", top: true },
+  { id: "alerts", icon: icons.bell, label: "Alerts", top: true }, */
+}
+export default App;
