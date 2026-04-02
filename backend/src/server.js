d1e251cf94361a4ed079672f52aeffd913b17324
injamamulhak0804 <injamamulhak080404@gmail.com>
2026-04-02 20:50:08 +0530

    feat: import the connectDB fn in server to connect DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import zentoraRoutes from "./routes/zentora.routes.js";
+import { connectDB } from "./config/connectDB.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
-
-//Routes 
+//Routes
 app.use("/api/v1", zentoraRoutes);
 
 app.get("/", (req, res) => {
- res.send("Please use /api/v1 for endpoints");
-})
+  res.send("Please use /api/v1 for endpoints");
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
