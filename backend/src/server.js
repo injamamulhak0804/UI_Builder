d1d9649bdb0d70fa160d825b94f361d03cdcd87e
injamamulhak0804 <injamamulhak080404@gmail.com>
2026-04-09 22:20:38 +0530

    feat: add cookie and cros fnpm package to read cookie

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import zentoraRoutes from "./routes/zentora.routes.js";
 import { connectDB } from "./config/connectDB.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.json());
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 //Routes
 app.use("/api/v1", zentoraRoutes);
